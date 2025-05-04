67cbaf2a100b2ab2b65c5b25caf11954bcdf3585
Ross Coull <coolssor@oe.tc>
2025-05-04 11:43:07 +0100

    Update worker.js
    Add favicon

--- worker.js
+++ worker.js
@@ -12,6 +12,7 @@ export default {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://get.bieber.party/favicon.ico" type="image/x-icon">
     <title>You've been Biebered!</title>
     <style>
         body {
